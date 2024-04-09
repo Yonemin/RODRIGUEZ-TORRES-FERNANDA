b6b22c9b5b09cbd9e7120c93a0828bae5d7fb88e
Yonemin <160188128+Yonemin@users.noreply.github.com>
2024-04-08 23:45:15 -0600

    Correccion error

--- Problemas JS/js/validacion2.js
+++ Problemas JS/js/validacion2.js
@@ -1,21 +1,17 @@
 function validar(formulario){
     if(!isNaN(formulario.venta1.value, formulario.venta2.value, formulario.venta3.value, formulario.sueldo.value)){
+        var ventaI=parseFloat(formulario.venta1.value);
+        var ventaII=parseFloat(formulario.venta2.value);
+        var ventaIII=parseFloat(formulario.venta3.value);
+        var sueldo=parseFloat(formulario.sueldo.value);
+        ventass=ventaI+ventaII+ventaIII;
+        comision=ventass*0.10;
+        sb=comision+sueldo;
+        alert("Su sueldo bruto es de: "+sb+".")
         return true;
     }
     else{
         alert("Por favor ingrese un valor numérico");
         return false;
     }
-}
-
-function calculo(formulario){
-    var ventaI=parseFloat(formulario.venta1.value);
-    var ventaII=parseFloat(formulario.venta2.value);
-    var ventaIII=parseFloat(formulario.venta3.value);
-    var sueldo=parseFloat(formulario.sueldo.value);
-    ventass=ventaI+ventaII+ventaIII;
-    comision=ventass*0.10;
-    sb=comision+sueldo;
-    alert("Su sueldo bruto es de: "+sb+".")
-    return true;
 }
