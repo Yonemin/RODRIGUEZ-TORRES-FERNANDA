e3258de7fed52b00f5e440bed0be341b2d184e19
Yonemin <160188128+Yonemin@users.noreply.github.com>
2024-04-09 02:04:39 -0600

    Ayuda no se por que no sirve el js del 4

--- Problemas JS/js/val4.js
+++ Problemas JS/js/val4.js
@@ -1,22 +1,14 @@
-function calificacionfinal(formulario){
-    if(!isNaN(p1, p2, p3, ef, tf)){
+function calfinal(formulario){
         var calif1=parseFloat(formulario.p1.value);
         var calif2=parseFloat(formulario.p2.value);
         var calif3=parseFloat(formulario.p3.value);
         var calif4=parseFloat(formulario.p4.value);
         var calif5=parseFloat(formulario.p5.value);
-        var parciales=(calif1+calif2+calif3);
-        parciales=parciales*0.55;
-        calif4=calif4*0.30;
-        calif5=calif5*0.15;
-        var final=parciales+calif4+calif5;
-        alert("Tu calificación final será de "+final+".");
-        var cfinal=Math.round(final);
-        alert("Tu calificación final será de "+cfinal+".");
+        var parciales1=(calif1+calif2+calif3)/3;
+        var parciales2=parciales1*0.55;
+        var calife=calif4*0.30;
+        var calift=calif5*0.15;
+        var final=parciales2+calife+calift;
+        alert("Tu calificación final es "+final+".")
         return true;
-    }
-    else{
-        alert("Por favor ingresa un valor numérico. Puedes usar decimales.");
-        return false;
-    }
 }
