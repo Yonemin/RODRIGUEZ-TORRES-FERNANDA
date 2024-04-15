79caff145b5d11f97bca13fb073d01907e2035a7
Yonemin <160188128+Yonemin@users.noreply.github.com>
2024-04-14 18:48:08 -0600

    Problemas 1 y 2 terminados
    Habia subido la version que no era y borre la que si era  : D

--- 05Algoritmia/js/funcionesalgoritmia.js
+++ 05Algoritmia/js/funcionesalgoritmia.js
@@ -1,19 +1,23 @@
 function problema1(){
-    
+    let texto = document.querySelector('#p1-input').value;
+    let separado = texto.split(" ");
+    separado.reverse();
+    let resultado = separado.join(" ");
+    document.querySelector('#p1-output').textContent = 'Palabras invertidas: ' + resultado;
 }
 
 function problema2(){
-    var p2_x1 = document.querySelector('#p2-x1').ariaValueMax;
-    var p2_x2 = document.querySelector('#p2-x1').ariaValueMax;
-    var p2_x3 = document.querySelector('#p2-x1').ariaValueMax;
-    var p2_x4 = document.querySelector('#p2-x1').ariaValueMax;
-    var p2_x5 = document.querySelector('#p2-x1').ariaValueMax;
+    var p2_x1 = document.querySelector('#p2-x1').value;
+    var p2_x2 = document.querySelector('#p2-x2').value;
+    var p2_x3 = document.querySelector('#p2-x3').value;
+    var p2_x4 = document.querySelector('#p2-x4').value;
+    var p2_x5 = document.querySelector('#p2-x5').value;
 
-    var p2_y1 = document.querySelector('#p2-y1').ariaValueMax;
-    var p2_y2 = document.querySelector('#p2-y1').ariaValueMax;
-    var p2_y3 = document.querySelector('#p2-y1').ariaValueMax;
-    var p2_y4 = document.querySelector('#p2-y1').ariaValueMax;
-    var p2_y5 = document.querySelector('#p2-y1').ariaValueMax;
+    var p2_y1 = document.querySelector('#p2-y1').value;
+    var p2_y2 = document.querySelector('#p2-y2').value;
+    var p2_y3 = document.querySelector('#p2-y3').value;
+    var p2_y4 = document.querySelector('#p2-y4').value;
+    var p2_y5 = document.querySelector('#p2-y5').value;
 
 //vamos a formar los vectores
 
